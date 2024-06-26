1adff98a7ac783e8b132dcf942dcd99765c647db
alexdrgpy06 <116830034+alexdrgpy06@users.noreply.github.com>
2024-06-26 03:19:50 -0400

    Update app.py to integrate fetching funcionarios and related news

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,13 @@
-from flask import Flask, jsonify from scraping.funcionarios import obtener_funcionarios, buscar_noticias
-
+from flask import Flask, jsonify
+from scraping.funcionarios import obtener_funcionarios
+from scraping.noticias import buscar_noticias
+import json
 app = Flask(__name__)
-
-@app.route('/api/funcionarios', methods=['GET'])
+@app.route('/api/funcionarios', methods='get')
 def get_funcionarios():
     funcionarios = obtener_funcionarios()
     for funcionario in funcionarios:
         funcionario['noticias'] = buscar_noticias(funcionario)
-    return jsonify(num=1})
-
+    return jsonify(funcionarios)
 if __name__ == '__main__':
     app.run(debug=True)
