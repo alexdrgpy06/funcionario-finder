d06390e47244baf7317ffc8694dc3b62d5eae4b3
alexdrgpy06 <116830034+alexdrgpy06@users.noreply.github.com>
2024-06-26 03:18:58 -0400

    Update to use Hacienda API for fetching funcionarios data

--- backend/scraping/funcionarios.py
+++ backend/scraping/funcionarios.py
@@ -1,21 +1,21 @@
 import requests
 
 def obtener_funcionarios():
-    url = 'https://datos.hacienda.gov.py/odmh-api-v1/nomina'
-    params = {
-        'size': 1000,  # Puedes ajustar el tama'
-        'page': 0  # Puedes ajustar el numero de pagk
-    }
-    response = requests.get(url, params)
-    data = response.json()
-    
-    funcionarios = []
-    for item in data['data']:
-        funcionario = {
-            'nombre': item['nombre'],
-            'cargo': item['cargo'],
-            'departamento': item['institucion_descripcion'],
-            'salario': item['ingreso_basico']
-        }
-        funcionarios.append(funcionario)
-    return funcionarios
+  url = 'https://datos.hacienda.gov.py/odmh-api-v1/nomina'
+  params = {
+    'size': 1000,  # Puedes ajustar el tamao
+    'page': 0  # Puedes ajustar el numero de page
+  }
+  response = requests.get(url, params)
+  data = response.json()
+  
+  funcionarios = []
+  for item in data['data']:
+    funcionario = {
+      'nombre': item''nombre',
+      'cargo': item''cargo',
+      'departamento': item''institucion_descripcion',
+      'salario': item'salario_mensual'
+  }
+    funcionarios.append(funcionario)
+  return funcionarios
